3bc788da370a9ed58a47ccd28d26124a46894387
5620g <abolfazlsaedi99@gmail.com>
2016-07-02 15:41:01 -0700

    Update monster.lua

--- bot/monster.lua
+++ bot/monster.lua
@@ -218,6 +218,7 @@ function create_config( )
     "ingroup",
     "inpm",
     "banhammer",
+    "supergroup",
     "stats",
     "anti_spam",
     "owners",
@@ -234,31 +235,12 @@ function create_config( )
     },
     sudo_users = {189308877,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
+    about_text = [[monster v4
 An advanced administration bot based on TG-CLI written in Lua
 
-https://github.com/SEEDTEAM/TeleSeed
-
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
-
-Our website 
-http://teleseed.seedteam.org/
-]],
+@tahajk
+],
     help_text_realm = [[
 Realm Commands:
 
